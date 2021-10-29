3c55af928cab52a36c4653afd4a6e271c81d9c05
alessandro migliore <90908133+alemigliore@users.noreply.github.com>
2021-10-29 12:33:26 +0200

    Add files via upload

---
+++ _teoria3_stringhe.py
@@ -0,0 +1,6 @@
+nome = input("dimmi il tuo nome: ")
+#anni = int(input("dimmi la tua età: "))
+print(f"il tuo nome è {nome}")
+print(f"il tuo nome inizia con {nome[0]}") #selezionare un carattere della stringa
+print(f"il tuo nome finisce con {nome[-1]}")
+print(f"la penultima lettera del tuo nome è {nome[-2]}")
