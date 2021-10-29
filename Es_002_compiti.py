6c7a4f06de134ae1379a54598de02e448deddc62
alessandro migliore <90908133+alemigliore@users.noreply.github.com>
2021-10-29 12:35:29 +0200

    Add files via upload

---
+++ Es_002_compiti.py
@@ -0,0 +1,7 @@
+a , b , stringa , f , ok  =  1 , 2 , 3 , 4 , "Ciao" , 2.1 , Vero
+
+print ( f" { a } tipo: { type ( a ) } " )
+print ( f" { b } tipo: { type ( b ) } " )
+print ( f" { stringa } tipo: { tipo ( stringa ) } " )
+print ( f" { f } tipo: { type ( f ) } " )
+print ( f" { ok } tipo: { type ( ok ) } " )
