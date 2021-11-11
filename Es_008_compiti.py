fbfdbe528e8fdb742fa72667c0cf0e1699a78c19
alessandro migliore <90908133+alemigliore@users.noreply.github.com>
2021-11-11 11:43:47 +0100

    Delete Es_008_compiti.py

--- Es_008_compiti.py
+++
@@ -1,7 +0,0 @@
-"""Crea un programma in Python in cui assegni una parola di almeno 8 lettere a una variabile stringa 
-e poi stampi tutta la parola con un carattere ? al posto della terza lettera."""
-
-stringa= "ciao e buona notte"
-print(stringa)
-stringa_nuova=stringa[:3]+"?"+stringa[4:]
-print(stringa_nuova)
