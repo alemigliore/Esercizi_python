09baca469ed31426625408961bc96d8e800b5eff
alessandro migliore <90908133+alemigliore@users.noreply.github.com>
2021-11-11 11:45:08 +0100

    Delete Es_005_compiti.py

--- Es_005_compiti.py
+++
@@ -1,4 +0,0 @@
-"""Crea un programma in Python in cui assegni una parola a una variabile stringa e poi stampi tutta la parola tranne le lettere iniziali e finali della parola."""
-
-stringa= "ciao"
-print(stringa[1:-1])
