d2d4efcf1c52808bc62e66112bd09182547ea86f
alessandro migliore <90908133+alemigliore@users.noreply.github.com>
2021-11-11 11:40:10 +0100

    Delete Es_023_compiti.py

--- Es_023_compiti.py
+++
@@ -1,9 +0,0 @@
-"""
-Scrivi un programma in Python che permetta all’utente di inserire un numero intero e una stringa: 
-concatena la stringa con sé stessa tante volte quante il numero e stampa il risultato.
-"""
-
-name=input("Dammi una parola: ")
-n=int(input("Numero di ripetizioni: "))
-nome2=name*n
-print(nome2)
