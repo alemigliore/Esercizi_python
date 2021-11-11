0fe5adb7393d8e96fc8df8f2fcda2779bd09bfa5
alessandro migliore <90908133+alemigliore@users.noreply.github.com>
2021-11-11 11:42:49 +0100

    Delete Es_018_compiti.py

--- Es_018_compiti.py
+++
@@ -1,8 +0,0 @@
-a = int(input("inserisci un numero: "))
-b = int(input("inserisci un numero: "))
-somQ = ((a*a)+(b*b))
-qSom = ((a + b)**2)
-diffQ = ((a*a)-(b*b))
-qDiff = ((a - b)**2)
-lista = (somQ, qSom, diffQ, qDiff)
-print(lista)
