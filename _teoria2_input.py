63831e20aaecab312b650e5056c8e9e4ae28a518
alessandro migliore <90908133+alemigliore@users.noreply.github.com>
2021-11-11 11:39:44 +0100

    Delete _teoria2_input.py

--- _teoria2_input.py
+++
@@ -1,9 +0,0 @@
-x = 3.1415 #un float
-
-#chiedere un numero in input e converte in float
-a = float(input("scrivi un numero decimale: "))
-
-y = x*a
-
-#f-string serve per stampare una stringa
-print(f"{x} moltiplicato per {a} fa {y}")
