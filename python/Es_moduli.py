9145812f3d032110959974a97bf7fc581012bc75
alessandro migliore <90908133+alemigliore@users.noreply.github.com>
2022-01-20 11:41:51 +0100

    Add files via upload

--- python/Es_moduli.py
+++ python/Es_moduli.py
@@ -1,16 +1,31 @@
-from xmlrpc.client import boolean
 import Es_pila_coda as pc  #alias / sinanimo
 
 def main():
     c = pc.Coda()
+    p = pc.Pila()
+    c1 = pc.Coda()
 
     risposta = "si"
+    counter = 0
     while(risposta == "si"):
         x = int(input("inserisci un numero: "))
+        counter += 1
         c.enqueue(x)
         risposta = input("ne vuoi inserire ancora? (si/no)")
     
     c.print()
+
+    for k in range(counter):
+        num = c.denqueue()
+        p.push(num)
+    
+    for j in range(counter):
+        num = p.pop()
+        c1.enqueue(num)
+    
+    c1.print()
+
+
 
 if __name__ == "__main__":
     main()
